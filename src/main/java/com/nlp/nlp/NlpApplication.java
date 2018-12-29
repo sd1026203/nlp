e511b5726c30e0db58d65d3a47d94ae6e23cc0ef
sd1026203 <sd1026203@126.com>
2018-12-29 16:52:39 +0800

    init

--- src/main/java/com/nlp/nlp/NlpApplication.java
+++ src/main/java/com/nlp/nlp/NlpApplication.java
@@ -3,11 +3,17 @@ package com.nlp.nlp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class NlpApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NlpApplication.class, args);
+        System.out.println((int)(("华".toCharArray())[0]));
+        DoubleArrayTrie doubleArrayTrie = new DoubleArrayTrie();
+        doubleArrayTrie.build(Arrays.asList("中华", "中华人民", "中华人民共和国","清华"));
+        System.out.println(doubleArrayTrie.exactMatchSearch("清华"));
+        System.out.println();
     }
 
 }
